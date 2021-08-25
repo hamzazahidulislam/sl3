59c71aef1e7f1cf57682b6f62e5ffa44d165c250
hamzazahidulislam <hamzazahidulislam444@gmail.com>
2021-08-25 21:38:47 +0300

    changes

--- nahid_vai/src/Main.java
+++ nahid_vai/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Profile nahid = new Profile("Nahid hasan");
-        if (nahid.login("nahid@gmail.com","123")){
+        if (nahid.login("nahid@gmail.com", "123")) {
             System.out.println(nahid.toString());
-        }else{
+        } else {
             System.out.println("Invalid credentials");
         }
     }
