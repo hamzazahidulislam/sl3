5da4c2f4f007bc005008d66588a9b3ce636d7ac0
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2023-08-13 10:27:38 +0600

    update

--- j/plugin.js
+++ j/plugin.js
@@ -1,4 +1,20 @@
 /** @format */
 
 console.clear()
-console.log('console')
+
+const value = ''
+
+// console.log('console')
+
+const fs = require('fs')
+fs.readFile('XN350_20230529123843447.txt', (err, inputD) => {
+  if (err) throw err
+  //   console.log(inputD.toString())
+  let text = inputD.toString()
+  //   console.log(text.search(/WBC/))
+  //   console.log(text.replace('', '').replace('', '').replace('☻', ''))
+
+  const p = 'Dog dog'
+  const regex = /|/gi
+  console.log(text.replace(regex, ''))
+})
