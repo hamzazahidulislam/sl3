3c4ab496fd47550ba9a949582f2e832a3e022d12
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2023-08-19 00:21:42 +0600

    updated

--- j/plugin.js
+++ j/plugin.js
@@ -4,10 +4,16 @@ console.clear()
 
 const value = ''
 
-const { Console, log } = require('console')
+// const { Console, log } = require('console')
+// const path = require('path')
+import path from 'path'
+import Watcher from 'watcher'
+// import chokidar from './chokidar'
 // console.log('console')
 
-const fs = require('fs')
+// const fs = require('fs')
+import axios from 'axios'
+import fs from 'fs'
 fs.readFile('XN350_20230529123843447.txt', async (err, inputD) => {
   if (err) throw err
   // console.log(await inputD.toString())
@@ -16,6 +22,7 @@ fs.readFile('XN350_20230529123843447.txt', async (err, inputD) => {
   //   console.log(text.replace('', '').replace('', '').replace('☻', ''))
 
   const p = 'Dog dog'
+  // we removed the unreadble charaters from this file
   const regex = /|/gi
   // console.log(text.replace(regex, ''))
 
@@ -34,28 +41,162 @@ fs.readFile('XN350_20230529123843447.txt', async (err, inputD) => {
 
   // Reading line by line
   // console.log(allLines)
-  // allLines.forEach((line) => {
-  //   if (line.length >= 40 && line.length <= 63) {
-  //     console.log('line gap')
-  //     console.log(line)
-  //   }
-  // })
+  var numberPattern = /\d+/g
+  var uniqueId = [] //this is hold our unique id
 
+  allLines.forEach((line) => {
+    // if (line.length >= 40 && line.length <= 200) {
+    //   console.log('line gap')
+    //   console.log(line)
+    // }
+    // hold uniqueID
+    if (line.length >= 300) {
+      // console.log('line gap')
+      const PipeRemove = line.toString().split('|')
+      const PipeSort = PipeRemove[(0, 3)]
+      const RemoveFreeSpace = PipeSort.replace(/[^\w\r\n]+/).toString()
+      const GetNumOnly = RemoveFreeSpace.replace(/^\D+\W*/g, '').toString()
+      const RemoveLastCircumflex =
+        GetNumOnly.replace(/\W*[ A-Za-z]+/g).toString()
+      const RemoveLast = RemoveLastCircumflex.match(numberPattern).toString()
+      uniqueId.push(RemoveLast)
+      // console.log(uniqueId.push(RemoveLast))
+    }
+  })
+  console.log(uniqueId[0])
+
+  // this line of code are use for find second uniqueId
+  const uniqueId2 = allLines[2].split('|')[4]
+  // hold uniqueID
+  console.log(uniqueId2)
+  const ObjectArrayMain = []
+  allLines.forEach((line) => {
+    if (line.length >= 40 && line.length <= 200) {
+      // console.log('line gap')
+      // console.log(line)
+
+      // this code will make key and value arrays for unique keys
+      const str = line
+      const converted = str.split('|')
+      const arraySplit = [...converted]
+
+      const keyValue = arraySplit[2]
+        .split(/[^A-Za-z]/g)
+        .toString()
+        .split(/\W*/)
+        .toString()
+        .replace(/[, ]+/g, '')
+        .trim()
+      // console.log(keyValue)
+      for (let index = 0; index < arraySplit.length; index++) {
+        const element = arraySplit[index]
+
+        ObjectArrayMain.push({ [keyValue]: element })
+        // we try to build a loop of array elements that array key and value array of array
+        // console.log((object['prop' + index] = element))
+      }
+    }
+  })
+  console.log(ObjectArrayMain)
+
+  // this code try creat a foler if folder doesn't exist
+  const DemoDbFilePath = `C:\\Users\\Hp\\Desktop\\sl3\\j`
+  const resolved = path.resolve(DemoDbFilePath)
+  const dir = `${resolved}` + '/completed'
+  console.log(dir)
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, {
+      recursive: true
+    })
+  }
+  const watcher = new Watcher(
+    './completed',
+    {},
+    (event, targetPath, targetPathNext) => {}
+  )
+  const folderPath = './completed'
+  // this code help me to rename file now need to transfer this file
+  console.log('Creating a new file...')
+  const newFile = fs.writeFileSync(
+    `./completed/${uniqueId[0] + `+` + uniqueId2}.txt`,
+    text
+  )
+  axios.post('http://localhost:5000/').catch(function (error) {
+    if (error.response) {
+      console.log(error.response.data)
+      console.log(error.response.status)
+      console.log(error.response.headers)
+    }
+  })
+
+  fs.readdir('./getFileList', (err, files) => {
+    files.forEach((file) => {
+      console.log(file)
+    })
+  })
+  // this code help me creat a new foler
+  // const fullPath = path.join(folderPath, newFile)
+  // fs.renameSync(fullPath, path.join(folderPath, newFile))
+
+  // this code required when app init
+  // const folderPath = './test'
+
+  // read all files in the directory
+  let filesArr = fs.readdirSync(folderPath)
+  console.log(filesArr)
+  // this is production code end
+  // console.log(uniqueId[0])
+
+  // console.log('something102asdfkj1948948'.match(numberPattern))
+  // *****
+  // const str = '6R|1|^^^^WBC^1|10.54|10*3/uL||N||F||||20230529114707'
+  // const converted = str.split('|')
+  // const arraySplit = [...converted]
+
+  // console.log(arraySplit)
+  // const ObjectArray = []
+  // for (let index = 0; index < arraySplit.length; index++) {
+  //   const element = arraySplit[index]
+  //   const object = { ugu: element }
+  //   ObjectArray.push([index, element])
+  //   // we try to build a loop of array elements that array key and value array of array
+  //   console.log((object['prop' + index] = element))
+  // }
+
+  // console.log(ObjectArray)
+  // *****
+
+  // this code will make key and value arrays for unique keys
   const str = '6R|1|^^^^WBC^1|10.54|10*3/uL||N||F||||20230529114707'
   const converted = str.split('|')
   const arraySplit = [...converted]
 
-  console.log(arraySplit)
+  const keyValue = arraySplit[2]
+    .split(/[^A-Za-z]/g)
+    .toString()
+    .split(/\W*/)
+    .toString()
+    .replace(/[, ]+/g, '')
+    .trim()
+  console.log(keyValue)
   const ObjectArray = []
   for (let index = 0; index < arraySplit.length; index++) {
     const element = arraySplit[index]
-    const object = { ugu: element }
-    ObjectArray.push([index, element])
+
+    ObjectArray.push({ [keyValue]: element })
     // we try to build a loop of array elements that array key and value array of array
     // console.log((object['prop' + index] = element))
   }
 
-  console.log(ObjectArray)
+  // this is for how they extract data from
+  // console.log(ObjectArray)
+  for (let elm of ObjectArray) {
+    for (const key in elm) {
+      // console.log(elm[key])
+    }
+    // console.log(elm)
+  }
 
   function Object(
     Object1,
@@ -117,18 +258,34 @@ fs.readFile('XN350_20230529123843447.txt', async (err, inputD) => {
   // const obj2 = Object.fromEntries(array)
   // console.log(obj2)
 
-  console.log('hi')
-  const chokidar = require('chokidar')
+  // console.log('hi')
+  // const chokidar = require('chokidar')
 
   // One-liner for current directory
-  chokidar
-    .watch('./test', {
-      ignoreInitial: true
-    })
-    .on('add', (path) => {
-      console.log(path)
-    })
+  // chokidar
+  //   .watch('./test', {
+  //     ignoreInitial: true
+  //   })
+  //   .on('add', (path) => {
+  //     console.log(path)
+  //   })
+  const watcherConnect = new Watcher('./completed', {
+    depth: 500,
+    depth: 2,
+    ignoreInitial: true,
+    persistent: true,
+    native: true
+  })
 
+  watcherConnect.on('all', (event, targetPath, targetPathNext) => {
+    console.log(event, 'this is event') // => could be any target event: 'add', 'addDir', 'change', 'rename', 'renameDir', 'unlink' or 'unlinkDir'
+    if (event === 'add') {
+      console.log(event, 'yes i am added')
+      console.log(targetPath, 'this is path') // => the file system path where the event took place, this is always provided
+    }
+    console.log(targetPath, 'this is path') // => the file system path where the event took place, this is always provided
+    console.log(targetPathNext, 'next teget path') // => the file system path "targetPath" got renamed to, this is only provided on 'rename'/'renameDir' events
+  })
   // let string = `test\g.ts Stats {
   //   dev: 4201496056,
   //   mode: 33206,
